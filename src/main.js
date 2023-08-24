eaa7f934d82b9040b1b63bfbe5bf7ed97c495af5
Abdullokh_dev <abdulloh3525Dev@gmail.com>
2023-08-24 14:55:46 +0300

    add some changes

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import './assets/css/bootstrap-grid.css'
+import './assets/css/main.css'
+import './assets/css/style.css'
 import App from './App.vue'
 
 createApp(App).mount('#app')
